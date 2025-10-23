8b4a89b49054b9ae144edc72efdf6c5328c81f20
Joel Criollo Abarca <98426429+criollojoel10@users.noreply.github.com>
2025-10-23 11:55:26 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,15 @@
-pluginManagement {repositories {
-    google {
-        content {
-            includeGroupByRegex("com\\.android.*")
-            includeGroupByRegex("com\\.google.*")
-            includeGroupByRegex("androidx.*")
-        }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
-    mavenCentral()
-    gradlePluginPortal()
-}
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // Añade esta línea para el repositorio de Xposed
-        maven { url = uri("https://api.xposed.info/") }
     }
 }
 rootProject.name = "inDriveAutopilot"
